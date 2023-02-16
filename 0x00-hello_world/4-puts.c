b0144639976104c5bae4c04ca1d3dadca8d3b39a
EDDARHMOMI-HICHAM <heddarhmoumi@gmail.com>
2023-02-16 04:49:13 -0800

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+/**
+ *this programme will print Programming is like building a multilingual puzzle
+ *return 0 in the finalle
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
