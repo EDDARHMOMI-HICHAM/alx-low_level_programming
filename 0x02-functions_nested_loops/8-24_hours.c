dd0d101b64658068077430c5a71d62ed421a7765
EDDARHMOMI-HICHAM <heddarhmoumi@gmail.com>
2023-02-21 09:54:19 -0800

    done

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,7 +14,7 @@ _putchar((hour / 10) + '0');
 _putchar((hour % 10) + '0');
 _putchar(':');
 _putchar((minutes / 10) + '0');
-_putchar((minutes / 10) + '0');
+_putchar((minutes % 10) + '0');
 _putchar('\n');
 }
 }
