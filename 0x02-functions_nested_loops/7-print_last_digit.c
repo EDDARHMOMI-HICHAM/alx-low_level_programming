6df8c8cb4898da75ae2f1b8ec5d23c4470940485
EDDARHMOMI-HICHAM <heddarhmoumi@gmail.com>
2023-02-21 09:39:42 -0800

    k dn

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,14 +3,14 @@
 /**
  * print_last_digit - prints the last digit of a number
  * @n: number to get last digit of
- *
  * Return: value of the last digit
  */
 int print_last_digit(int n)
 {
-int last_digit;
+int last_digit = n % 10;
 
-hlast_digit = _abs(n % 10);
+if (last_digit < 0)
+last_digit *= -1;
 _putchar(last_digit + '0');
-return (last_digit);
+return (0);
 }
