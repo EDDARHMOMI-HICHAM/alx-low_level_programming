ab92f46ca56b2eb65b7bf3610169f1c031f0cbae
EDDARHMOMI-HICHAM <heddarhmoumi@gmail.com>
2023-02-21 09:25:47 -0800

    done

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _isalpha - checks for alphabetic character
  * @c: character to check
@@ -12,6 +13,7 @@ if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 return (1);
 }
 else
-{return (0);
+{
+return (0);
 }
 }
