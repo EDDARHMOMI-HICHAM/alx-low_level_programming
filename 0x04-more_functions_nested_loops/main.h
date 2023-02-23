e3f58f3f6430635a4ee0b525260922091ad9b7d3
EDDARHMOMI-HICHAM <heddarhmoumi@gmail.com>
2023-02-23 09:16:28 -0800

    push

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,6 +6,7 @@ int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_line(int n);
+void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 int _putchar (char c);
