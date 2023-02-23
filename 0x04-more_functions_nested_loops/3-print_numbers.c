abc028d33e1c83c324833b95e101c096dffd6050
EDDARHMOMI-HICHAM <heddarhmoumi@gmail.com>
2023-02-23 06:01:00 -0800

    3. The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio>
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ */
+
+void print_numbers(void)
+{
+for (int i = 0; i <= 9; i++)
+{
+_putchar(i + '0');
+
+}
+_putcha('\n');
+
+}
