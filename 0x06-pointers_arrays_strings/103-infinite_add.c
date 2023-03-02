21d419121da0ad72d5597ca77b0dc40477caaf0d
EDDARHMOMI-HICHAM <heddarhmoumi@gmail.com>
2023-03-02 11:55:54 -0800

    done

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,63 +1,46 @@
 #include "main.h"
-
+#include <string.h>
 /**
  * infinite_add - adds two numbers
+ *
  * @n1: first number
  * @n2: second number
- * @r: buffer to store result
- * @size_r: size of buffer
- * Return: pointer to result, or 0 if result can't be stored in buffer
+ * @r: buffer to store the result
+ * @size_r: size of the buffer
+ *
+ * Return: pointer to the result, or 0 if result cannot be stored in r
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int len1 = 0, len2 = 0, i = 0, j = 0, k = 0, carry = 0, sum = 0;
-
-/* find length of n1 and n2 */
-while (n1[len1])
-len1++;
-while (n2[len2])
-len2++;
-
-/* check if result can be stored in buffer */
-if (len1 + len2 >= size_r)
+int len1 = strlen(n1);
+int len2 = strlen(n2);
+int carry = 0;
+int i, j;
+if (len1 + 1 > size_r || len2 + 1 > size_r)
 return (0);
-
-/* add n1 and n2 starting from least significant digit */
-for (i = len1 - 1, j = len2 - 1, k = 0; k < size_r - 1 && (i >= 0 || j >= 0); i--, j--, k++)
+r[size_r - 1] = '\0';
+for (i = len1 - 1, j = len2 - 1; i >= 0 || j >= 0; i--, j--)
 {
-sum = carry;
-
-if (i >= 0)
-sum += n1[i] - '0';
-if (j >= 0)
-sum += n2[j] - '0';
-
+int digit1 = (i >= 0) ? n1[i] - '0' : 0;
+int digit2 = (j >= 0) ? n2[j] - '0' : 0;
+int sum = digit1 + digit2 + carry;
 if (sum > 9)
 {
 carry = 1;
 sum -= 10;
 }
 else
+{
 carry = 0;
-
-r[k] = sum + '0';
 }
-if (carry)
+r[size_r - 2 - (i > j ? i : j)] = sum + '0';
+}
+if (carry == 1)
 {
-if (k >= size_r - 1)
+if (size_r <= len1 + 1 || size_r <= len2 + 1)
 return (0);
-r[k++] = '1';
-}
-
-r[k] = '\0';
-
-/* reverse result */
-for (i = 0, j = k - 1; i < j; i++, j--)
-{
-char tmp = r[i];
-r[i] = r[j];
-r[j] = tmp;
-}
-
+r[0] = '1';
 return (r);
+}
+return (&r[size_r - 1 - (i > j ? i : j)]);
 }
