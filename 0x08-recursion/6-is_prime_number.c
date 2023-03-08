df417ceaccf35297bb65c7dc937735f7c6fc10bb
EDDARHMOMI-HICHAM <heddarhmoumi@gmail.com>
2023-03-08 15:37:29 -0800

    6. Inception. Is it possible?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -4,22 +4,21 @@
  *
  * Return: 1 if n is prime, 0 otherwise
  */
- 
 int is_prime_helper(int n, int lo);
 
 int is_prime_number(int n)
 {
 if (n < 2)
-return 0;
+return (0);
 if (n == 2 || n == 3)
-return 1;
+return (1);
 if (n % 2 == 0 || n % 3 == 0)
-return 0;
-return is_prime_helper(n, 5);
+return (0);
+return (is_prime_helper(n, 5));
 }
 
 /**
- * is_prime_helper - checks if n is divisible by any odd integer between lo and sqrt(n)
+ * is_prime_helper - checks if n is divisible by
  * @n: the number to check
  * @lo: the lowest odd integer to check
  *
@@ -28,8 +27,8 @@ return is_prime_helper(n, 5);
 int is_prime_helper(int n, int lo)
 {
 if (lo * lo > n)
-return 1;
+return (1);
 if (n % lo == 0 || n % (lo + 2) == 0)
-return 0;
-return is_prime_helper(n, lo + 6);
+return (0);
+return (is_prime_helper(n, lo + 6));
 }
