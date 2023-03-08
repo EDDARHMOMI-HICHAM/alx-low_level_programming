c627c41f793dded958333575b533e52f74c9ee10
EDDARHMOMI-HICHAM <heddarhmoumi@gmail.com>
2023-03-08 14:40:25 -0800

    edit more 1 and push 2

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,14 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - prints a string in reverse
- * @s : for alphabe
+ * _print_rev_recursion - Prints a string in reverse.
+ * @s: The string to print.
  */
 void _print_rev_recursion(char *s)
 {
-if (*s == '\n')
-{
-_putchar('\n');
+if (*s == '\0')
 return;
-}
+
 _print_rev_recursion(s + 1);
 _putchar(*s);
 }
