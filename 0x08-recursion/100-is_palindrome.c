292accce173090dbad93c5c9aafb879a00bebabb
EDDARHMOMI-HICHAM <heddarhmoumi@gmail.com>
2023-03-08 16:05:06 -0800

    tst

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,3 +1,5 @@
+#include <string.h>
+
 /**
  * is_palindrome - checks if a string is a palindrome
  *
@@ -8,13 +10,12 @@
 int is_palindrome(char *s)
 {
 int len = strlen(s);
-int i;
 
 if (len <= 1)
 return (1);
 
 if (s[0] == s[len - 1])
-return (is_palindrome(s + 1) && is_palindrome(s, len - 2));
+return (is_palindrome(s + 1) && is_palindrome(s + 1, len - 2));
 
 return (0);
 }
