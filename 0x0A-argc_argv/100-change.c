6d38f652dd8a0bb55dfa47433feac7cb943eb40a
EDDARHMOMI-HICHAM <heddarhmoumi@gmail.com>
2023-03-10 15:28:05 -0800

    Minimal Number of Coins for Change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -16,7 +16,7 @@ int cents, coins = 0;
 if (argc != 2)
 {
 printf("Error\n");
-return 1;
+return (1);
 }
 
 cents = atoi(argv[1]);
@@ -24,7 +24,7 @@ cents = atoi(argv[1]);
 if (cents < 0)
 {
 printf("0\n");
-return 0;
+return (0);
 }
 
 while (cents > 0)
@@ -45,5 +45,5 @@ coins++;
 
 printf("%d\n", coins);
 
-return 0;
+return (0);
 }
