e61678a0c5058c8eef1d9efc4b571a2da8d46a59
EDDARHMOMI-HICHAM <heddarhmoumi@gmail.com>
2023-03-13 15:41:23 -0700

    It isn't the mountains ahead to climb that wear you out; it's the pebble in your shoe

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,22 +1,34 @@
-#include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
- * main - check the code for ALX School students.
+ * argstostr - Concatenates all the arguments of the program.
+ * @ac: The number of arguments.
+ * @av: An array of pointers to the arguments.
  *
- * Return: Always 0.
+ * Return: A pointer to a new string containing the concatenated arguments,
+ * separated by a newline character, or NULL if ac == 0, av == NULL, or if
+ * memory allocation fails.
  */
-int main(int ac, char *av[])
+char *argstostr(int ac, char **av)
 {
-char *s;
-
-s = argstostr(ac, av);
-if (s == NULL)
+int i, j, len = 0, total_len = 0;
+char *str;
+if (ac == 0 || av == NULL)
+return (NULL);
+for (i = 0; i < ac; i++)
 {
-return (1);
+for (j = 0; av[i][j] != '\0'; j++)
+len++;
+total_len += len + 1;
+len = 0;
 }
-printf("%s", s);
-free(s);
-return (0);
+str = malloc(sizeof(char) * total_len);
+if (str == NULL)
+return (NULL);
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j] != '\0'; j++)
+*str++ = av[i][j];
+*str++ = '\n';
+}
+*str = '\0';
+return (str - total_len);
 }
