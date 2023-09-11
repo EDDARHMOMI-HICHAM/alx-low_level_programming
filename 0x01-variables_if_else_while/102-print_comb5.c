b63f40db0e273d58945cab6952ddcd6d7bd79412
EDDARHMOMI-HICHAM <heddarhmoumi@gmail.com>
2023-09-11 16:26:14 +0100

    print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 int num1, num2;
-for (num1 = 0; num1 <= 99; num1++)
+for (num1 = 0; num1 <= 9; num1++)
 {
-for (num2 = num1; num2 <= 99; num2++)
+for (num2 = 0; num2 <= 9; num2++)
 {
-putchar((num1 / 10) + '0');
-putchar((num1 % 10) + '0');
-putchar(' ');
-putchar((num2 / 10) + '0');
-putchar((num2 % 10) + '0');
-if (num1 != 99 || num2 != 99)
+printf("%02d %02d", num1, num2);
+if (num1 != 9 || num2 != 9)
 {
-putchar(',');
-putchar(' ');
+printf(", ");
 }
 }
 }
-putchar('\n');
+printf("\n");
 return (0);
 }
