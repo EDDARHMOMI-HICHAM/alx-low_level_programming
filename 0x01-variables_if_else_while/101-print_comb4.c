d221247314d2367ae1bfbe7f17765c1ead2d40a5
EDDARHMOMI-HICHAM <heddarhmoumi@gmail.com>
2023-09-11 16:28:11 +0100

    print_comb5

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int num1, num2;
+
+for (num1 = 0; num1 <= 9; num1++)
+{
+for (num2 = 0; num2 <= 9; num2++)
+{
+printf("%02d %02d", num1, num2);
+
+if (num1 != 9 || num2 != 9)
+{
+printf(", ");
+}
+}
+}
+printf("\n");
+return (0);
+}
