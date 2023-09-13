c0931d388de611b76eb5b6b76fc29d16226e34e0
EDDARHMOMI-HICHAM <heddarhmoumi@gmail.com>
2023-09-13 13:14:43 +0100

    push putchar task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes a character to standard output
+ * @c: The character to print
+ *
+ * Return: 1 on success, -1 on error
+ */
+int _putchar(char c)
+{
+    return write(1, &c, 1);
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('\n');
+    return (0);
+}
