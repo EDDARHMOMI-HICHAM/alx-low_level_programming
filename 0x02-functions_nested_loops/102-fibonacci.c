f725692e80e4c8fc4ec624570a87b98d6712aec4
EDDARHMOMI-HICHAM <heddarhmoumi@gmail.com>
2023-09-16 14:54:05 +0100

    push task 103

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 Fibonacci numbers, separated by comma and space
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+int i;
+long int fib[50] = {1, 2};
+
+for (i = 2; i < 50; i++)
+{
+fib[i] = fib[i - 1] + fib[i - 2];
+}
+
+for (i = 0; i < 49; i++)
+{
+printf("%ld, ", fib[i]);
+}
+
+printf("%ld\n", fib[49]);
+
+return (0);
+}
