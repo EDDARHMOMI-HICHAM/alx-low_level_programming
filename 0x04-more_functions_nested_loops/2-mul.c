c81540109b7117573608c1719c7da367c3dcd431
EDDARHMOMI-HICHAM <heddarhmoumi@gmail.com>
2023-09-16 17:29:22 +0100

    push task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * mul - multiplies two integers
+ *
+ * @a: first number
+ *
+ * @b: second nember
+ *
+ * Return: the resolt of a*b
+ */
+int mul(int a, int b)
+{
+int result;
+
+result = a * b;
+
+return (result);
+}
