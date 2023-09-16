f846d7e85600ab648f6c87800ebf4796e55696a0
EDDARHMOMI-HICHAM <heddarhmoumi@gmail.com>
2023-09-16 17:22:29 +0100

    push 8 task

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square of a given size
+ * @size: size of the square
+ *
+ * Return: void
+ */
+void print_square(int size)
+{
+int row, column;
+
+if (size <= 0)
+{
+_putchar('\n');
+return;
+}
+
+for (row = 0; row < size; row++)
+{
+for (column = 0; column < size; column++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
