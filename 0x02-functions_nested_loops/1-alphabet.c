177d51fa00f55029fd7589c9d3253d660c9f5fa7
EDDARHMOMI-HICHAM <heddarhmoumi@gmail.com>
2023-09-16 14:22:17 +0100

    push 2 task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the lower alphapet
+ */
+
+void print_alphabet(void)
+{
+char alpha1;
+
+for (alpha1 = 'a'; alpha1 <= 'z'; alpha1++)
+{
+_putchar(alpha1);
+}
+_putchar('\n');
+
+}
