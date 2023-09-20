251d5da94b3428fc59caa9fb4866f88efe22cc2b
EDDARHMOMI-HICHAM <heddarhmoumi@gmail.com>
2023-09-20 04:49:19 -0700

    string_toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @str: the string to modify
+ *
+ * Return: a pointer to the modified string
+ */
+char *string_toupper(char *str)
+{
+char *ptr = str;
+
+while (*ptr)
+{
+if (*ptr >= 'a' && *ptr <= 'z')
+{
+*ptr = *ptr - 'a' + 'A';
+}
+ptr++;
+}
+return (str);
+}
