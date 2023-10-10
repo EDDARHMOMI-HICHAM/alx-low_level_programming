9ff3e04df7cd106ae0152098c0c6ad74fc635bf5
EDDARHMOMI-HICHAM <heddarhmoumi@gmail.com>
2023-10-10 19:12:26 +0100

    push init_dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes a struct dog
+ * @d: pointer to the struct dog to initialize
+ * @name: name to initialize
+ * @age: age to initialize
+ * @owner: owner to initialize
+ *
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d == NULL)
+return;
+
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
