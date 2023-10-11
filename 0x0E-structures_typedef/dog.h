2a9ff2ee6d27ea464718dc15537932163e62ebc9
EDDARHMOMI-HICHAM <heddarhmoumi@gmail.com>
2023-10-11 10:44:28 -0700

    push

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-
 
 /**
  * struct dog - a struct representing a dog
@@ -19,4 +17,5 @@ float age;
 char *owner;
 };
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
