75a4abdd5425394388b5a69eb38c687c6901e8c9
EDDARHMOMI-HICHAM <heddarhmoumi@gmail.com>
2023-10-11 10:37:56 -0700

    push edit

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,12 +9,12 @@
  *
  * Return: void
  */
+{
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-if (d == NULL)
-return;
-
+if (d != NULL)
+{
 d->name = name;
-d.age = age;
+d->age = age;
 d->owner = owner;
 }
