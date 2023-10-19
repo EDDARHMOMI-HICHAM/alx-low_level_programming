aebc3662631ccf32cbde38159c8a2e0fd243874a
hix.mox <hichameddarhmoum20@gmail.com>
2023-10-19 22:14:36 +0100

    push task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -7,23 +7,23 @@
  *
  * Return: The address of the new element, or NULL if it failed
  */
- 
+
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *n_node;
 
 	if (str == NULL)
-		return NULL;
+		return (NULL);
 
 	n_node = malloc(sizeof(list_t));
 	if (n_node == NULL)
-		return NULL;
+		return (NULL);
 
 	n_node->str = strdup(str);
 	if (n_node->str == NULL)
 	{
 		free(n_node);
-		return NULL;
+		return (NULL);
 	}
 
 	n_node->len = strlen(str);
