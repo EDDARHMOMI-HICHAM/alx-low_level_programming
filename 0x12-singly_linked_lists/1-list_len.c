9adab74a7d61d70b205b10229cd60e59ec4d855b
hix.mox <hichameddarhmoum20@gmail.com>
2023-10-19 19:47:40 +0100

    push the task 2

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -9,13 +9,13 @@
  */
 size_t list_len(const list_t *h)
 {
-    size_t len = 0;
+	size_t len = 0;
 
-    while (h != NULL)
-    {
-	    len++;
-	    h = h->next;
-    }
+	while (h != NULL)
+	{
+		len++;
+		h = h->next;
+	}
 
-    return (len);
+	return (len);
 }
