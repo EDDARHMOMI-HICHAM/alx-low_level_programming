098bb18173e41817cb7700a6bd46b83dfe765eba
hix.mox <hichameddarhmoum20@gmail.com>
2023-10-20 03:11:44 +0100

    push tasks

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,36 +11,43 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *n_node, *temp;
 
-	if (str == NULL)
+	if (str == NULL) {
 		return (NULL);
+	}
 
-	n_node = malloc(sizeof(list_t);
-	if (n_node == NULL)
+	if (*head == NULL) {
+		*head = malloc(sizeof(list_t));
+		if (*head == NULL) {
+			return (NULL);
+		}
+		(*head)->str = strdup(str);
+		if ((*head)->str == NULL) {
+			free(*head);
+			return (NULL);
+		}
+		(*head)->len = strlen(str);
+		(*head)->next = NULL;
+		return (*head);
+	}
+
+	n_node = malloc(sizeof(list_t));
+	if (n_node == NULL) {
 		return (NULL);
-
+	}
 	n_node->str = strdup(str);
-	if (n_node->str == NULL)
-	{
+	if (n_node->str == NULL) {
 		free(n_node);
 		return (NULL);
 	}
-
 	n_node->len = strlen(str);
 	n_node->next = NULL;
 
-	if (*head == NULL)
-	{
-		*head = n_node;
-	}
-	else
-	{
-		temp = *head;
-		while (temp->next != NULL)
-		{
+	temp = *head;
+	while (temp->next != NULL) {
 		temp = temp->next;
-		}
-		temp->next = n_node;
 	}
+	temp->next = n_node;
 
 	return (n_node);
 }
+
