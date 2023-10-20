d17dbfdd259c2e214897ec22a5fd191cd5788147
hix.mox <hichameddarhmoum20@gmail.com>
2023-10-20 03:22:36 +0100

    push 100 task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * before_main - Function executed before main.
+ *
+ * This function prints a message before the main function is executed.
+ */
+void __attribute__((constructor)) before_main() {
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
