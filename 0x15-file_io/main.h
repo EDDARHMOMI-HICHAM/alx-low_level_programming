de6c09a28f46b607627ddbbe7140712c745caf44
hix.mox <heddarhmoumi@gmail.com>
2023-10-30 16:57:58 +0100

    push task 4

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#define BUFFER_SIZE 1024
 
 int append_text_to_file(const char *filename, char *text_content);
 int create_file(const char *filename, char *text_content);
