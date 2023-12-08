5ffa502241d490f8990a496bb6445b7994cd9508
hixmox <heddarhmoumi@gmail.com>
2023-12-07 18:14:27 -0800

    push task 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -10,39 +10,39 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *current_node, *temp;
-    unsigned int i = 0;
+	dlistint_t *current_node, *temp;
+	unsigned int i = 0;
 
-    if (head == NULL || *head == NULL)
-        return -1;
+	if (head == NULL || *head == NULL)
+		return (-1);
 
-    current_node = *head;
+	current_node = *head;
 
-    if (index == 0)
-    {
-        *head = (*head)->next;
-        if (*head != NULL)
-            (*head)->prev = NULL;
-        free(current_node);
-        return 1;
-    }
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(current_node);
+		return (1);
+	}
 
-    while (current_node != NULL && i < index - 1)
-    {
-        current_node = current_node->next;
-        i++;
-    }
+	while (current_node != NULL && i < index - 1)
+	{
+		current_node = current_node->next;
+		i++;
+	}
 
-    if (current_node == NULL || current_node->next == NULL)
-        return -1;
+	if (current_node == NULL || current_node->next == NULL)
+		return (-1);
 
-    temp = current_node->next;
-    current_node->next = temp->next;
+	temp = current_node->next;
+	current_node->next = temp->next;
 
-    if (temp->next != NULL)
-        temp->next->prev = current_node;
+	if (temp->next != NULL)
+		temp->next->prev = current_node;
 
-    free(temp);
+	free(temp);
 
-    return 1;
+	return (1);
 }
